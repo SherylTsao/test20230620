d8d47a7589981f99ee6336abe5ca9b23d0b3d732
LAPTOP-DQT65H7B\User <sha2672tlpu@gmail.com>
2023-06-20 12:34:03 +0800

    init

---
+++ src/main/java/com/example/test20230620/Test20230620Application.java
@@ -0,0 +1,13 @@
+package com.example.test20230620;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20230620Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test20230620Application.class, args);
+	}
+
+}
